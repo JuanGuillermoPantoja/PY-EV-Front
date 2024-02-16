55e5847ba71e031105bf47074553a7d88fffca90
unknown <SenaManana@ARMAPRRQDFSD974.FormacionSena.RED>
2024-02-16 10:25:01 -0500

    up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,27 +5,41 @@ import { NavLink } from "react-router-dom";
 function HomePage() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-1/2 bg-[#FFEEB3] h-full flex items-center justify-center">
-          <img  src={logoeventBrew} alt="" />
+      <div className="flex justify-center items-center h-screen
+      max-[480px]:flex-col">
+        <div className="w-1/2 bg-[#FFEEB3] h-full flex items-center justify-center first-letter
+         max-[480px]:h-1/6">
+          <img className="max-[600px]:w-4/5 max-[480px]:w-1/3" src={logoeventBrew} alt="" />
         </div>
-        <div className="w-1/2 bg-[#AC703E] h-full flex flex-col justify-around items-center">
-          <h2 className="text-7xl">Bienvenidos</h2>
-          <div className="w-2/3 flex justify-between h-14">
+        <div className="w-1/2 bg-[#AC703E] h-full flex flex-col justify-around items-center
+        max-[480px]:justify-evenly">
+          <h2 className="text-7xl  max-[768px]:text-6xl max-[600px]:text-5xl max-[480px]:text-4xl">Bienvenidos</h2>
+          <div className="w-2/3 flex justify-between h-14 max-[600px]:w-3/4">
             <NavLink
-              className="bg-[#FFEEB3] text-black h-full w-1/3 text-2xl border rounded-2xl flex justify-center items-center font-bold hover:shadow-2xl hover:text-3xl"
-              to="/login-clients"
+              className="bg-[#FFEEB3] text-black h-full w-1/3 text-2xl border rounded-2xl flex justify-center items-center font-bold text-center hover:shadow-2xl hover:text-3xl
+              max-[1024px]:text-lg max-[1024px]:hover:text-xl max-[1024px]:h-5/6
+              max-[768px]:w-2/5
+              max-[600px]:w-[48%]
+              max-[480px]:text-sm"
+              to="/login"
             >
               Iniciar sesión
             </NavLink>
             <NavLink
-              className="bg-[#FFEEB3] text-black h-full w-1/3 text-2xl border rounded-2xl flex justify-center items-center font-bold hover:shadow-2xl hover:text-3xl"
-              to="/register"
+              className="bg-[#FFEEB3] text-black h-full w-1/3 text-2xl border rounded-2xl flex justify-center items-center font-bold hover:shadow-2xl hover:text-3xl
+              max-[1024px]:text-lg max-[1024px]:hover:text-xl max-[1024px]:h-5/6
+              max-[768px]:w-2/5
+              max-[600px]:w-[48%]
+              max-[480px]:text-sm"
             >
               Registrate
             </NavLink>
           </div>
-          <p className="w-4/5 h-1/3 text-center text-2xl">
+          <p className="w-4/5 h-1/3 text-center text-2xl
+          max-[1024px]:text-xl
+          max-[768px]:text-lg
+          max-[600px]:text-lg
+          max-[480px]:text-sm">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
             libero atque, reprehenderit fugiat voluptate consectetur velit
             debitis ex modi quisquam, incidunt doloribus aut vitae excepturi
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -24,13 +24,23 @@ function RegisterPage() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen bg-[url('https://i.ibb.co/LQf91TG/fondo-EB.webp')] bg-cover">
-        <h2 className="text-7xl text-[#FFEEB3]">REGISTRATE</h2>
-        <div className="flex justify-center items-center h-4/5 w-[80%]">
-          <div className="flex flex-col justify-center items-center w-1/2">
-            <Link to="/">
-              <img className="1/2" src={logoeventBrew} alt="" />
+        <h2 className="text-7xl text-[#FFEEB3]
+        max-[1024px]:text-6xl
+        max-[480px]:text-4xl
+        max-[480px]:">REGISTRATE</h2>
+        <div className="flex justify-center items-center h-4/5 w-[80%]
+        max-[768px]:w-[90%]
+        max-[480px]:flex-col">
+          <div className="flex flex-col justify-center items-center w-1/2
+          max-[1024px]:pb-12
+          max-[480px]:hidden">
+            <Link  to="/">
+              <img className="1/2 max-[1024px]:w-4/5 max-[768px]:m-auto" src={logoeventBrew} alt="" />
             </Link>
-            <p className="bg-[#4A2D0B] rounded-full py-1 px-4 text-[#FFEEB3] text-2xl ">
+            <p className="bg-[#4A2D0B] rounded-full py-1 px-4 text-[#FFEEB3] text-2xl
+            max-[1024px]:text-xl max-[1024px]:px-6
+            max-[600px]:text-lg max-[600px]:px-5
+            max-[480px]:text-sm max-[480px]:px-2">
               ¿Ya tienes cuenta? Inicia sesión aquí.{" "}
               <NavLink
                 className="text-[#AC703E] underline hover:font-bold"
@@ -41,10 +51,13 @@ function RegisterPage() {
             </p>
           </div>
           <form
-            className="flex flex-col items-center w-1/2 h-[70%]"
+            className="flex flex-col items-center w-1/2 h-[70%]
+            max-[480px]:w-3/5"
             onSubmit={onSubmit}
           >
-            <div className="bg-[#0000004f] rounded-xl flex flex-col justify-center items-center w-4/5 h-full">
+            <div className="bg-[#0000004f] rounded-xl flex flex-col justify-center items-center w-4/5 h-full
+            max-[1024px]:h-[90%]
+            max-[480px]:h-full max-[480px]:w-full">
               {registerErrors.map((error, i) => (
                 <div className="bg-red-500 text-white w-2/3 text-lg" key={i}>
                   {error}
@@ -52,7 +65,9 @@ function RegisterPage() {
               ))}
 
               <input
-                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold"
+                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold
+                max-[768px]:w-3/4
+                max-[480px]:text-base"
                 type="text"
                 {...register("username", { required: true })}
                 placeholder="Nombres:"
@@ -63,7 +78,9 @@ function RegisterPage() {
               )}
 
               <input
-                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold"
+                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold
+                max-[768px]:w-3/4
+                max-[480px]:text-base"
                 type="email"
                 {...register("email", { required: true })}
                 placeholder="Correo:"
@@ -74,7 +91,9 @@ function RegisterPage() {
               )}
 
               <input
-                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold"
+                className="my-2 w-2/3 h-12 text-lg bg-[#FFEEB3] text-[#AC703E] pl-2 font-bold placeholder-[#AC703E] placeholder:font-bold
+                max-[768px]:w-3/4
+                max-[480px]:text-base"
                 type="password"
                 {...register("password", { required: true })}
                 placeholder="Contraseña:"
@@ -85,7 +104,10 @@ function RegisterPage() {
               )}
 
               <button
-                className="bg-[#FFEEB3] text-[#AC703E] text-2xl font-bold h-12 w-1/4 rounded-full mt-2 hover:bg-[#AC703E] hover:text-[#FFEEB3] duration-300"
+                className="bg-[#FFEEB3] text-[#AC703E] text-2xl font-bold h-12 w-1/4 rounded-full mt-2 hover:bg-[#AC703E] hover:text-[#FFEEB3] duration-300
+                max-[1024px]:w-1/3
+                max-[600px]:text-lg max-[600px]:h-10
+                max-[480px]:text-xl"
                 type="submit"
               >
                 Enviar
