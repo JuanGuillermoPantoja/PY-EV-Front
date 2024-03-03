bd78145e206d136e6d4b81cb089d34b74cc24a66
Sergio Diaz <sydiazcastro@gmail.com>
2024-03-02 19:26:07 -0500

    up

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -48,24 +48,24 @@ function NavbarHome() {
         <>
           <nav className="bg-white w-full flex flex-col sm:flex-row justify-between items-center px-2 h-[70px] border-b-[1px] border-gray-200">
             {/* <img className='w-[5.5%]' src={logoEventBrew} /> */}
-            <h1 className="text-textBlack  text-base sm:text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl">
+            <h1 className="text-textBlack  text-xl sm:text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl">
               <span className="text-acent">E</span>v
               <span className="text-primary">en</span>ts 
               <span className="text-acent">B</span>r
               <span className="text-primary">ew</span>
             </h1>
-            <ul className="flex justify-center items-center gap-x-1 text-sm sm:text-base md:text-lg lg:text-xl text-black h-full lg:gap-x-2">
+            <ul className="flex justify-center items-center gap-x-1 text-xs sm:text-base md:text-lg lg:text-xl text-black h-full lg:gap-x-2">
               <li
                 id="btn-events"
-                className="bg-acent active:bg-amber-700 text-primary p-2 rounded-xl hover:animate-bouncing hover:animate-iteration-count-infinite duration-100"
+                className="bg-acent active:bg-amber-700 text-primary p-2 h-2/3 flex items-center rounded-xl hover:animate-bouncing hover:animate-iteration-count-infinite duration-100"
               >
                 <Link to="/">eventos</Link>
               </li>
-              <li className="bg-acent text-primary p-2 rounded-xl hover:animate-bouncing hover:animate-iteration-count-infinite duration-100">
+              <li className="bg-acent text-primary p-2 rounded-xl h-2/3 flex items-center hover:animate-bouncing hover:animate-iteration-count-infinite duration-100">
                 <Link to="/login-clients">Inicia sesión</Link>
               </li>
               <div className="h-[60%] w-[1px] bg-gray-200"></div>
-              <li className="bg-acent text-primary p-2 rounded-xl hover:animate-bouncing hover:animate-iteration-count-infinite duration-100">
+              <li className="bg-acent text-primary p-2 rounded-xl h-2/3 flex items-center hover:animate-bouncing hover:animate-iteration-count-infinite duration-100">
                 <Link to="/register-clients">Registrarse</Link>
               </li>
             </ul>
