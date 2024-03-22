569ac2b9a61ac1574ed4d044e845e56ac0a515fb
SergioDiazCastro <sydiazcastro@gmail.com>
2024-03-22 07:07:41 -0500

    up

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -82,8 +82,8 @@ function Chat() {
       )}
       {isModalOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex items-end justify-end bottom-0 animate-slide-in-bottom animate-duration-1000">
-          <div className='bg-amber-950 text-[#f8d98f] border-2 border-b-0 rounded-b-none flex flex-col items-center p-8 rounded-lg w-[80%] sm:w-[70%] lg:w-[20%]'>
-            <h1 className="text-2xl my-4">Charla con nosotros!</h1>
+          <div className='bg-amber-950 text-[#f8d98f] border-2 border-b-0 rounded-b-none flex flex-col items-center p-8 rounded-lg w-[80%] min-[500px]:w-[60%] sm:w-[70%] md:w-[60%] lg:w-[40%]'>
+            <h1 className="text-lg md:text-2xl my-4">Charla con nosotros!</h1>
 
             <div className='animate-fade-in flex justify-center items-center flex-col w-full bg-amber-100 rounded-t-md'>
               <SimpleBar autoHide className="w-full" style={{ maxHeight: 300 }}>
